2a75bfe482124b399fe9491e8cd8fd5c40b4e360
Renan Bonette <renan.bonette@gmail.com>
2019-10-31 20:45:21 +0100

    Update server.js

--- server.js
+++ server.js
@@ -87,10 +87,10 @@ app.get('/', function (req, res) {
       if (err) {
         console.log('Error running count. Message:\n'+err);
       }
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+      res.render('index.html', { pageCountMessage : process.env.IP, dbInfo: dbDetails });
     });
   } else {
-    res.render('index.html', { pageCountMessage : null});
+    res.render('index.html', { pageCountMessage : process.env.IP});
   }
 });
 
